db31e459f5257bd47b86ea521de2ab9cee1ce60d
Sim <simchasitzer1@gmail.com>
2019-06-26 20:19:28 -0400

    change variable to hash, move logic from view to controller

--- app/controllers/api/game_controller.rb
+++ app/controllers/api/game_controller.rb
@@ -4,7 +4,15 @@ class Api::GameController < ApplicationController
     render 'game.json.jb'
   end
   def guessing_game
-    @guess = params["guess"].to_i
+    guess = params["guess"].to_i
+    correct_number = 79
+    if guess == correct_number
+      @message = {message: "You got the right number!"}
+    elsif guess < correct_number
+      @message = {message: "Too low!"}
+    elsif guess > correct_number
+      @message = {message: "Too high!"}
+    end
     render 'guessing_game.json.jb'
   end
 end
